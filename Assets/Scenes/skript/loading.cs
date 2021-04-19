1ef9601a436b34e18a8515e7db7b4671cc39c131
Adi Murdayani <adimurdayani@gmail.com>
2021-04-19 13:20:49 +0800

    Initial commit

---
+++ Assets/Scenes/skript/loading.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class loading : MonoBehaviour
+{
+    public Transform LoadingBar;
+
+    [SerializeField]
+    private float waktu;
+
+    [SerializeField]
+    private float kecepatan;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(waktu < 100)
+        {
+            //fungsi untuk mengatur waktu
+            waktu += kecepatan * Time.deltaTime;
+            Debug.Log((int)waktu);
+        }
+        else
+        {
+            //fungsi untuk menghubungkan splash screen dan halaman menu utama
+            Application.LoadLevel("h_menu_utama");
+        }
+
+        LoadingBar.GetComponent<Image>().fillAmount = waktu / 100;
+
+    }
+}
